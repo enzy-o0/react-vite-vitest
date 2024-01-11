ee29c3cf18a7e34b8d02a52ca25fe910bc2883bc
enzy-o0 <oej3659@gmail.com>
2024-01-11 14:14:23 +0900

    feat: 테스트 항목 button color 값 변경 및 eslit, prettier 설정

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,35 +1,94 @@
-import { render, screen, fireEvent } from "@testing-library/react"
-import App from "./App"
-import { logRoles } from "@testing-library/dom";
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+import { logRoles } from '@testing-library/dom';
+import { kebabCaseToTitleCase } from './helpers';
 
-test("App contains correct heading", () => {
-    render(<App />)
+test('App contains correct heading', () => {
+    render(<App />);
     const headingElement = screen.getByText(/learn React/i);
-    expect(headingElement).toBeInTheDocument()
-})
+    expect(headingElement).toBeInTheDocument();
+});
 
 // test("button starts with correct label and color", () => {
-test("button click flow", () => {
+test('button click flow', () => {
     // const { container } = render(<App />);
     render(<App />);
     // logRoles(container);
     // name은 버튼 텍스트
-    const buttonElement = screen.getByRole("button", { name: /blue/i });
-    expect(buttonElement).toHaveClass("red");
-});
-
-test("button starts with correct text", () => {
-    render(<App />);
-    const buttonElement = screen.getByRole("button");
-    expect(buttonElement).toHaveClass("red");
+    const buttonElement = screen.getByRole('button', { name: /blue/i });
+    // expect(buttonElement).toHaveClass('red');
+    expect(buttonElement).toHaveClass('medium-violet-red');
 
     fireEvent.click(buttonElement);
 
     expect(buttonElement).toHaveTextContent(/red/i);
-    // expect(buttonElement).toHaveClass("blue");
+    expect(buttonElement).toHaveClass("midnight-blue");
+});
+
+test('button starts with correct text', () => {
+    render(<App />);
+    const buttonElement = screen.getByRole('button');
+    expect(buttonElement).toHaveClass('red');
+
+    fireEvent.click(buttonElement);
+
+    expect(buttonElement).toHaveTextContent(/red/i);
+    expect(buttonElement).toHaveClass('blue');
     // expect(buttonElement).toHaveStyle({ "background-color": "blue" });
 });
 
 // test("button has correct label and color after click", () => {
-
 // });
+
+test('checkbox flow', () => {
+    render(<App />);
+
+    const buttonElement = screen.getByRole('button', { name: /blue/i });
+    const checkBoxElement = screen.getByRole('checkbox', {
+        name: /disable button/i,
+    });
+
+    expect(buttonElement).toBeEnabled();
+    expect(checkBoxElement).not.toBeChecked();
+
+    fireEvent.click(checkBoxElement);
+    expect(buttonElement).toBeDisabled();
+    expect(buttonElement).toHaveClass('gray');
+
+    fireEvent.click(checkBoxElement);
+    expect(buttonElement).toBeEnabled();
+    // expect(buttonElement).toHaveClass("red");
+    expect(buttonElement).toHaveClass('medium-violet-red');
+});
+
+test('checkbox flow after button click', () => {
+    render(<App />);
+
+    const buttonElement = screen.getByRole('button', { name: /blue/i });
+    const checkBoxElement = screen.getByRole('checkbox', {
+        name: /disable button/i,
+    });
+
+    fireEvent.click(buttonElement);
+
+    fireEvent.click(checkBoxElement);
+    expect(buttonElement).toBeDisabled();
+    expect(buttonElement).toHaveClass('gray');
+
+    fireEvent.click(checkBoxElement);
+    expect(buttonElement).toBeEnabled();
+    // expect(buttonElement).toHaveClass('blue');
+    expect(buttonElement).toHaveClass('midnight-blue');
+});
+
+describe('kebabCaseToTitleCase', () => {
+    test('works for no hyphens', () => {
+        expect(kebabCaseToTitleCase('red')).toBe('Red');
+    });
+    test('works for one hyphens', () => {
+        expect(kebabCaseToTitleCase('midnight-blue')).toBe('Midnight Blue');
+    });
+    test('works for multiple hyphens', () => {
+        expect(kebabCaseToTitleCase('medium-violet-red')).toBe('Medium Violet Red');
+    });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import './App.css'
+import { kebabCaseToTitleCase } from './helpers';
 
 function App() {
-  const [buttonColor, setButtonColor] = React.useState('red');
-  const nextColor = buttonColor === 'red' ? 'blue': 'red';
+  const [disabled, setDisabled] = React.useState(false);
+  // const [buttonColor, setButtonColor] = React.useState('red');
+  // const nextColor = buttonColor === 'red' ? 'blue': 'red';
+  const [buttonColor, setButtonColor] = React.useState('medium-violet-red');
+  const nextColorClass = buttonColor === 'medium-violet-red' ? 'midnight-blue': 'medium-violet-red';
+  const nextColorTitleCase = kebabCaseToTitleCase(nextColorClass);
+  const className = disabled ? 'gray' : buttonColor;
 
   function onClickButton() {
-    setButtonColor(nextColor);
+    setButtonColor(nextColorClass);
+  }
+
+  function onChangeCheckBox(e) {
+    setDisabled(e.target.checked)
   }
 
   return (
     <div>
       <h1>I'm gonna learn React Testing Library</h1>
-      <button className={buttonColor} onClick={onClickButton}>Change to {nextColor}</button>
+      {/* <button disabled={disabled} className={className} onClick={onClickButton}>Change to {nextColorClass}</button> */}
+      <button disabled={disabled} className={className} onClick={onClickButton}>Change to {nextColorTitleCase}</button>
+      <br />
+      <input type='checkbox' id='disable-button-checkbox' defaultChecked={disabled}  onChange={onChangeCheckBox}/>
+      <label htmlFor='disable-button-checkbox'>Disable button</label> 
+      <br />
     </div>
   )
 }
